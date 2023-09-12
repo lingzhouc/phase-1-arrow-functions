be62ba7ff1b110733c5a587814b8827011dadd7e
LingZhouChen <lzcciao@gmail.com>
2023-09-11 20:36:35 -0400

    complete lab

---
+++ index.js
@@ -0,0 +1,8 @@
+// divide arrow fn
+const divide = (num1, num2) => num1 / num2;
+
+// square arrow fn; multiply times itself
+const square = (num) => num ** num;
+
+// add arrow fn
+const add = (num1, num2) => num1 + num2;
